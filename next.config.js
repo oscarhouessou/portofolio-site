c842934bb381f974ded1ff36e2155dfcd47b7636
oscar Houessou <114242920+oscarhouessou@users.noreply.github.com>
2025-06-07 16:21:31 +0100

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     domains: ['drive.google.com'],
+    unoptimized: true,
   },
+  basePath: '/portofolio-site',
   // Add experimental features to help with hydration
   experimental: {
     // This helps with hydration issues
